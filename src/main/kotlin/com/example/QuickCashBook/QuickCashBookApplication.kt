85ee305655876bbaa67e3e8295adc4ba97e179bb
AiriNarita <airiswim.kitty@gmail.com>
2024-01-03 14:57:33 +0900

    feat: first commit

---
+++ src/main/kotlin/com/example/QuickCashBook/QuickCashBookApplication.kt
@@ -0,0 +1,11 @@
+package com.example.QuickCashBook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuickCashBookApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuickCashBookApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/QuickCashBook/QuickCashBookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.QuickCashBook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuickCashBookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
