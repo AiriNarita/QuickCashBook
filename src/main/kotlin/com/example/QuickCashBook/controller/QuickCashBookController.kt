0375f7111e87906f0ca299ccc95e83c53d882274
AiriNarita <airiswim.kitty@gmail.com>
2024-01-03 16:12:02 +0900

    feat: add post method

--- src/main/kotlin/com/example/QuickCashBook/controller/QuickCashBookController.kt
+++ src/main/kotlin/com/example/QuickCashBook/controller/QuickCashBookController.kt
@@ -1,10 +1,17 @@
 package com.example.QuickCashBook.controller
 
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/webhook")
 class QuickCashBookController {
+
+    @PostMapping("/")
+    fun messsagePost(){
+    }
+
+
 
 }
