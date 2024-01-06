4805808d7c81b6f3b91118f2de0ecac158261793
AiriNarita <airiswim.kitty@gmail.com>
2024-01-06 13:22:54 +0900

    feat: add message tamplate model

--- src/main/kotlin/com/example/QuickCashBook/MessagesService/MessagesService.kt
+++ src/main/kotlin/com/example/QuickCashBook/MessagesService/MessagesService.kt
@@ -1,4 +1,4 @@
-package MessagesService
+package com.example.QuickCashBook.MessagesService
 
 import org.springframework.stereotype.Service
 
---
+++ src/main/kotlin/com/example/QuickCashBook/Model/GenreButtonsTemplate.kt
@@ -0,0 +1,9 @@
+package com.example.QuickCashBook.Model
+
+import com.fasterxml.jackson.annotation.JsonTypeName
+
+
+/** ButtonsTemplate */
+@JsonTypeName("buttons")
+class GenreButtonsTemplate {
+}
