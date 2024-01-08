4e220529eb584ce4fbe60ebd8fc9b4de80df05d0
AiriNarita <airiswim.kitty@gmail.com>
2024-01-08 14:04:43 +0900

    fix: changing confirm action to buttom action

--- src/main/kotlin/com/example/QuickCashBook/QuickCashBookApplication.kt
+++ src/main/kotlin/com/example/QuickCashBook/QuickCashBookApplication.kt
@@ -11,6 +11,7 @@ import com.linecorp.bot.webhook.model.PostbackEvent
 import com.linecorp.bot.webhook.model.TextMessageContent
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.net.URI
 
 
 fun main(args: Array<String>) {
@@ -30,6 +31,7 @@ open class QuickCashBookApplication(
         when (event?.postback?.data.toString()) {
             "A" -> return TextMessage("AAAA！")
             "B" -> return TextMessage("BBBB")
+            "C" -> return TextMessage("CCCC")
         }
         return TextMessage("?")
     }
@@ -55,19 +57,26 @@ open class QuickCashBookApplication(
             }
 
             val numbersOnly = messagesService.extractNumbers(originalMessageText)
+            val A_Action = PostbackAction("食費", "A", "A", null, PostbackAction.InputOption.OPENKEYBOARD, null)
+            val B_Action = PostbackAction("交通費", "B", "B", null, PostbackAction.InputOption.OPENKEYBOARD, null)
+            val C_Action = PostbackAction("通費", "C", "C", null, PostbackAction.InputOption.OPENKEYBOARD, null)
 
-            val confirmTemplate = ConfirmTemplate(
-                "￥" + "$numbersOnly" + "は何のジャンル？",
+            //　ここをbutton templateに変えたい
+            val buttonTemplate = ButtonsTemplate(
+                URI.create("https://任意の画像URL.jpg"), null, "cover", "#000000",
+                "￥" + "$numbersOnly" + "は何のジャンル？", "ひとつ選んでね",
+                null,
                 listOf(
-                    PostbackAction("食費", "A", "A", null, PostbackAction.InputOption.OPENKEYBOARD, null),
-                    PostbackAction("交通費", "B", "B", null, PostbackAction.InputOption.OPENKEYBOARD, null),
+                    A_Action,
+                    B_Action,
+                    C_Action
                 )
             )
 
             messagingApiClient.replyMessage(
                 ReplyMessageRequest(
                     event.replyToken,
-                    listOf(TemplateMessage("質問だよ", confirmTemplate)),
+                    listOf(TemplateMessage("質問だよ", buttonTemplate)),
                     false
                 )
             )
